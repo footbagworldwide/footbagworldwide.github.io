f0adc6c322f046cef79c767762cbad333b6d5758
Matt Kemmer <mkemmer@higherlogic.com>
2023-04-01 13:46:05 -0400

    -refactor router to wrap page layout around main pages but not 404

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
+import './assets/App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import './assets/App.css';
-import Header from  './pages/site-wrapper/header/header.js';
-import Footer from './pages/site-wrapper/footer/footer.js';
-import { AppRoutes, ScrollToTop } from './router';
+import AppRouter from './router.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <Header />
-        <AppRoutes />
-      <Footer />
-    </BrowserRouter>
+    <AppRouter />
   </React.StrictMode>
 );
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+
+import PageLayout from "./pages/page-layout/page-layout.js";
 
 import HomePage from './pages/home/home-page.js';
 import EventsPage from './pages/events/events-page.js';
@@ -9,20 +11,6 @@ import FreestylePage from "./pages/games/freestyle/freestyle-page.js";
 import NetPage from "./pages/games/net/net-page.js";
 import AboutPage from "./pages/about/about-page.js";
 import NotFoundPage from "./pages/404/404-page.js";
-
-function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="/games/kicking" element={<KickingGamesPage />} />
-      <Route path="/games/freestyle" element={<FreestylePage />} />
-      <Route path="/games/net" element={<NetPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  );
-}
 
 function ScrollToTop({ children }) {
   const { pathname } = useLocation();
@@ -39,4 +27,29 @@ function ScrollToTop({ children }) {
   return children;
 }
 
-export { AppRoutes, ScrollToTop };
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<PageLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/games/kicking" element={<KickingGamesPage />} />
+        <Route path="/games/freestyle" element={<FreestylePage />} />
+        <Route path="/games/net" element={<NetPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;

73f9f4f01525b48769f582c8454a317f1b21d436
Matt Kemmer <mkemmer@higherlogic.com>
2023-04-07 10:00:24 -0400

    -improve responsivity of footbag game section of home page

--- src/pages/home/sections/intro/intro-section.js
+++ src/pages/home/sections/intro/intro-section.js
@@ -1,35 +1,22 @@
 import './intro-section.css';
 import { freestyleData, hackySackData, netData } from "../../../../data/games/footbag-games-data.js";
 import { ButtonLink, ImageLink } from "../../../../components/links/link-components";
-import { useDesktopDisplay } from '../../../../hooks/display-hook';
 
-function FootbagGames() {
-  const FootbagGameItem = (props) => {
-    const footbagGame = props.footbagGame;
-
-    return (
-      <div className='footbag-game-container'>
-        <div className="footbag-game">
-          <ImageLink
-            route={footbagGame.pageRoute}
-            src={footbagGame.icon}
-            className="footbag-game-icon"
-          />
-          <h3>{footbagGame.title}</h3>
-          <div>{footbagGame.description.short}</div>
-        </div>
-        <ButtonLink route={footbagGame.pageRoute}><strong>MORE</strong></ButtonLink>
-      </div>
-    );
-  }
-
-  const className = useDesktopDisplay() ? "footbag-games-desktop-container" : "";
+function FootbagGameItem(props) {
+  const footbagGame = props.footbagGame;
 
   return (
-    <div id="footbag-games-container" className={className}>
-      <FootbagGameItem footbagGame={hackySackData} />
-      <FootbagGameItem footbagGame={freestyleData} />
-      <FootbagGameItem footbagGame={netData} />
+    <div className='footbag-game-container'>
+      <div className="footbag-game">
+        <ImageLink
+          route={footbagGame.pageRoute}
+          src={footbagGame.icon}
+          className="footbag-game-icon"
+        />
+        <h3>{footbagGame.title}</h3>
+        <div>{footbagGame.description.short}</div>
+      </div>
+      <ButtonLink route={footbagGame.pageRoute}><strong>MORE</strong></ButtonLink>
     </div>
   );
 }
@@ -47,7 +34,11 @@ function IntroSection() {
 					and competitive. Footbag is played all over the world, bonding together a dedicated
 					community around their love for a unique and amazing sport.
 			</p>
-			<FootbagGames />
+			<div className='footbag-games-container'>
+        <FootbagGameItem footbagGame={hackySackData} />
+        <FootbagGameItem footbagGame={freestyleData} />
+        <FootbagGameItem footbagGame={netData} />
+      </div>
 		</div>
 	);
 }

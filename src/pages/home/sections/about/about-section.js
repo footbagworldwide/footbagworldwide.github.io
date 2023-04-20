f4c2f9ce281bb3baf1748bb08390c245e7dc9014
Matt Kemmer <mkemmer@higherlogic.com>
2023-04-20 15:43:49 -0400

    -fix up about section display

--- src/pages/home/sections/about/about-section.js
+++ src/pages/home/sections/about/about-section.js
@@ -2,15 +2,20 @@ import './about-section.css';
 import historyImage from '../../../../assets/images/wfc-logos/WFC2018.jpg';
 import { ButtonLink } from '../../../../components/links/link-components';
 
-function AboutSectionContainer(props) {
-  const className = props.className ?? '';
-  const containerClassName = props.containerClassName ?? '';
+function AboutSectionItem(props) {
+  const title = props.title;
+  const description = props.description;
+  const imageSrc = props.imageSrc;
+  const imageAlt = props.imageAlt;
 
   return (
-    <div className={`about-section-item ${className}`}>
-      <h2 className="about-section-item-header">{props.title}</h2>
-      <div className={`about-section-content ${containerClassName}`}>
-        { props.children }
+    <div className="about-section-item">
+      <h2>{title}</h2>
+      <div className="about-section-content">
+        <div className='about-section-image-container'>
+          <img className="about-section-image" src={imageSrc} alt={imageAlt} />
+        </div>
+        <p className="about-section-text">{description}</p>
       </div>
       <div className="about-button-container">
         <ButtonLink route="/about">MORE</ButtonLink>
@@ -19,44 +24,21 @@ function AboutSectionContainer(props) {
   );
 }
 
-function FootbagHistory() {
-  return (
-    <AboutSectionContainer title="HISTORY OF FOOTBAG" >
-      <div className='about-section-image-container'>
-        <img className="about-section-image" src={historyImage} alt="Footbag history" />
-      </div>
-      <p className="about-section-text">
-        Footbag roots can be found deep in the ancient
-        cultures of Asia and the Americas, with
-        similar games being played as far back as
-        3000 years ago. Its current form was established
-        in North America in the 1970s, and it
-        has spread globally and evolved into many
-        forms since.
-      </p>
-    </AboutSectionContainer>
-  );
-}
-
-function AboutIfpa(props) {
-  return (
-    <AboutSectionContainer title="ABOUT IFPA" className={props.className}>
-      <div className='about-section-image-container'>
-        <img className="about-section-image" src={historyImage} alt="Footbag history" />
-      </div>
-      <p className="about-section-text">
-        IFPA (International Footbag Players' Association, Inc.) is a volunteer-run, charitable non-profit corporation,
-        dedicated to the growth of footbag play world-wide as lifetime recreation and as an amateur, competitive sport.
-      </p>
-    </AboutSectionContainer>
-  );
-}
-
 function AboutSection() {
   return (
     <div className='home-page-section page-subsection about-section-container'>
-      <FootbagHistory />
-      <AboutIfpa className="about-section-container-ifpa" />
+      <AboutSectionItem
+        title="HISTORY OF FOOTBAG"
+        description="Footbag roots can be found deep in the ancient cultures of Asia and the Americas, with similar games being played as far back as 3000 years ago. Its current form was established in North America in the 1970s, and it has spread globally and evolved into many forms since."
+        imageSrc={historyImage}
+        imageAlt="Footbag history"
+      />
+      <AboutSectionItem
+        title="ABOUT IFPA"
+        description="IFPA (International Footbag Players' Association, Inc.) is a volunteer-run, charitable non-profit corporation, dedicated to the growth of footbag play world-wide as lifetime recreation and as an amateur, competitive sport."
+        imageSrc={historyImage}
+        imageAlt="Footbag history"
+      />
     </div>
   );
 }

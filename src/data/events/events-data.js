0fd6d7dad9077315b051e4b105731099396fe0a0
Matt Kemmer <mkemmer@higherlogic.com>
2023-05-27 20:42:35 -0400

    -fill in worlds playlist urls

--- src/data/events/events-data.js
+++ src/data/events/events-data.js
@@ -25,63 +25,63 @@ const events = [
 		title: "WFC 2022",
 		location: "Krakow, POLAND",
 		resultsLink: "http://www.footbag.org/events/show/1646387871",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfG1yYjnAGsOgMSA33kAfA7b",
 		imagePath: logo_2022
 	},
 	{
 		title: "WFC 2021",
 		location: "ONLINE",
 		resultsLink: "http://www.footbag.org/events/show/1623054449",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfFtMMGAcO0CDDFG_Q6h-dLC",
 		imagePath: logo_2021
 	},
 	{
 		title: "WFC 2020",
 		location: "ONLINE",
 		resultsLink: "http://www.footbag.org/reference/-/IFPA_Online_Freestyle_Footbag_World_Championships_2020",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfE5IyhSGCNxI1Lqe_OOL_a5",
 		imagePath: logo_2020
 	},
 	{
 		title: "WFC 2019",
 		location: "Warsaw, POLAND",
 		resultsLink: "http://www.footbag.org/events/show/1547415984m",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfGBQnFMrUqHzb3aRfCYabGq",
 		imagePath: logo_2019
 	},
 	{
 		title: "WFC 2018",
 		location: "Sofia, BULGARIA",
 		resultsLink: "http://www.footbag.org/events/show/1519215398",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfFSwa1G9dhMFv6HPLLSk7sR",
 		imagePath: logo_2018
 	},
 	{
 		title: "WFC 2017",
 		location: "Portland, USA",
 		resultsLink: "http://www.footbag.org/events/show/1471686537",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfGB85OROtCFu9afzoGozbxU",
 		imagePath: logo_2017
 	},
 	{
 		title: "WFC 2016",
 		location: "Trnava, SLOVAKIA",
 		resultsLink: "http://www.footbag.org/events/show/1449259560",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfFDFwYzPbt0kgx_YudsZw7m",
 		imagePath: logo_2016
 	},
 	{
 		title: "WFC 2015",
 		location: "Copenhagen, DENMARK",
 		resultsLink: "http://www.footbag.org/events/show/1417735560",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfF8i-95tiVOINzIQJL2idTC",
 		imagePath: logo_2015
 	},
 	{
 		title: "WFC 2014",
 		location: "Paris, FRANCE",
 		resultsLink: "http://www.footbag.org/events/show/1391189882",
-		videoLink: "https://www.google.com",
+		videoLink: "https://www.youtube.com/playlist?list=PLxP76n1rTXfG3AO-shoVYbPWM7CWgwuhD",
 		imagePath: logo_2014
 	},
 	{
@@ -93,4 +93,4 @@ const events = [
 	}
 ];
 
-export { events};
+export { events };

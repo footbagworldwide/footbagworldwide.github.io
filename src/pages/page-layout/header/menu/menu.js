b1a72c16e4fa02a13062259798169973994a6353
Matt Kemmer <mkemmer@higherlogic.com>
2023-06-05 18:48:59 -0400

    change submenu to open on click rather than hover for more consistent mobile experience

--- src/pages/page-layout/header/menu/menu.js
+++ src/pages/page-layout/header/menu/menu.js
@@ -2,7 +2,7 @@ import './menu.css';
 import { Link, useLocation } from 'react-router-dom';
 import menuItems from './menu-data';
 import { useDesktopDisplay } from '../../../../hooks/display-hook';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 function MenuItem(props) {
   const menuItem = props.menuItem;
@@ -14,14 +14,28 @@ function MenuItem(props) {
   );
 }
 
+function closeOnOutsideClick(menuRef, menuVisible, setMenuVisible) {
+  const closeOpenMenu = (e) => {
+    if (menuRef.current && menuVisible && !menuRef.current.contains(e.target)){
+      setMenuVisible(false)
+    }
+  };
+
+  document.addEventListener('mousedown', closeOpenMenu);
+}
+
 function Submenu(props) {
+  const [showSubmenu, setShowSubmenu] = useState(false);
+
   const submenu = props.submenu;
   const submenuContainerClassName = props.submenuContainerClassName;
 
-  return (
-    <div>
-      <div className="submenu">
-        <strong className="menu-item">{submenu.label}</strong> <SubmenuArrow />
+  const submenuRef = useRef();
+  closeOnOutsideClick(submenuRef, showSubmenu, setShowSubmenu);
+
+  const SubmenuContainer = () => {
+    if(showSubmenu) {
+      return (
         <div className={"submenu-item-container " + submenuContainerClassName}>
           {
             submenu.menuItems.map(menuItem =>
@@ -31,16 +45,38 @@ function Submenu(props) {
             )
           }
         </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  const SubmenuArrow = () =>{
+    if(showSubmenu) {
+      return (
+        <>
+          <i className="fa-solid fa-chevron-down submenu-arrow"></i>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <i className="fa-solid fa-chevron-right submenu-arrow"></i>
+        </>
+      );
+    }
+  }
+
+  return (
+    <div ref={submenuRef}>
+      <div
+        className={`submenu ${showSubmenu ? "submenu-active" : ""}`}
+        onClick={() => { setShowSubmenu(!showSubmenu) }}
+      >
+        <strong className="menu-item">{submenu.label}</strong> <SubmenuArrow />
+        <SubmenuContainer />
       </div>
     </div>
-  );
-}
-
-function SubmenuArrow() {
-  return (
-    <>
-      <i className="fa-solid fa-chevron-right submenu-arrow submenu-arrow-right"></i><i className="fa-solid fa-chevron-down submenu-arrow submenu-arrow-down"></i>
-    </>
   );
 }
 
@@ -64,12 +100,16 @@ function MobileMenu() {
   const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
 
+  // close mobile menu on page change
   useEffect(() => {
     setShowMenu(false);
   }, [location.pathname]);
 
+  const mobileMenuRef = useRef();
+  closeOnOutsideClick(mobileMenuRef, showMenu, setShowMenu);
+
   const menu = () => (
-    <div className="mobile-menu-container">
+    <div ref={mobileMenuRef} className="mobile-menu-container">
       <RawMenu mobile={true} />
     </div>
   );

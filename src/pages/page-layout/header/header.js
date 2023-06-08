bda45304dbc4dee0d4c4d68814f933d0d3482921
Matt Kemmer <matthewkemmer@gmail.com>
2023-06-08 13:27:17 -0400

    make logo a link to home page on mobile

--- src/pages/page-layout/header/header.js
+++ src/pages/page-layout/header/header.js
@@ -5,16 +5,6 @@ import { EmailLink, ImageLink } from '../../../components/links/link-components.
 import Menu from './menu/menu';
 import emailIcon from '../../../assets/images/icons/envelope.svg';
 
-function SiteLogo() {
-  const logoAltText = "Logo for footbagworldwide";
-
-  if(useDesktopDisplay()) {
-    return <ImageLink className="logo-image" src={logo} alt={logoAltText} route="/" />
-  } else {
-    return <img className="logo-image" src={logo} alt={logoAltText} />
-  }
-}
-
 function Header() {
   const headerClassName = useDesktopDisplay() ? 'header-container_desktop' : 'header-container_mobile fixed-header-space';
 
@@ -23,7 +13,7 @@ function Header() {
       className={`header-container ${headerClassName}`}
     >
       <div></div>
-      <div><SiteLogo /></div>
+      <div><ImageLink className="logo-image" src={logo} alt="Logo for footbagworldwide" route="/" /></div>
       <Menu />
       { useDesktopDisplay() && <div><EmailLink><img id='email-icon' src={emailIcon} alt="Envelope icon" /></EmailLink></div> }
     </nav>

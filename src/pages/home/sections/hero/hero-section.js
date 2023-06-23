f205359c64977e3cc02c1470502634c0e98b7a60
Matt Kemmer <matthewkemmer@gmail.com>
2023-06-23 09:58:32 -0400

    -replace temp gif with community pic

--- src/pages/home/sections/hero/hero-section.js
+++ src/pages/home/sections/hero/hero-section.js
@@ -1,13 +1,14 @@
 import './hero-section.css';
 import { Video, VideoType } from "../../../../components/video/video";
 import { useTouchDisplay } from "../../../../hooks/display-hook";
+import communityPhoto from '../../../../assets/images/ifpa-community.jpg';
 
 function HeroSectionMobile() {
   // auto-play videos are not supported on a mobile device
   
   return (
     <div>
-      <img className="hero-gif" src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif" alt="Gif of footbag players" />
+      <img className="hero-gif" src={communityPhoto} alt="Picture of Worlds 2016 attendees" />
     </div>
   );
 }
